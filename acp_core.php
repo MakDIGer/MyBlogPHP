5a6e7779cea57ac75617f503ecdd5c51e09ae800
Makarin Nikolay <makarin@kgdnet.ru>
2015-07-31 12:17:20 +0200

    Settings is ready in ACP

--- acp_core.php
+++ acp_core.php
@@ -21,6 +21,7 @@ switch ($acp_page) {
 	case 'rmprojects' : include_once 'systems/acp/rmprojects.php'; require_once 'templates/'.TEMPLATE.'/acp/rmprojects.php'; break;
 	case 'addprojects' : require_once 'templates/'.TEMPLATE.'/acp/head.php'; require_once 'templates/'.TEMPLATE.'/acp/sidebar.php'; include_once 'systems/acp/addprojects.php'; require_once 'templates/'.TEMPLATE.'/acp/addprojects.php'; break;
 	case 'editprojects' : require_once 'templates/'.TEMPLATE.'/acp/head.php'; require_once 'templates/'.TEMPLATE.'/acp/sidebar.php'; include_once 'systems/acp/editprojects.php'; require_once 'templates/'.TEMPLATE.'/acp/editprojects.php'; break;
+	case 'settings' : require_once 'templates/'.TEMPLATE.'/acp/head.php'; require_once 'templates/'.TEMPLATE.'/acp/sidebar.php'; include_once 'systems/acp/settings.php'; require_once 'templates/'.TEMPLATE.'/acp/settings.php'; break;
 	default: require_once 'templates/'.TEMPLATE.'/404.php';
 }
 
--- templates/default/acp/settings.php
+++ templates/default/acp/settings.php
@@ -13,21 +13,47 @@
 							<h2>Настройки портала</h2>
 							<p></p>
 						</header>
-						<p>
-							Основной домен: <a href="<?php echo $settings['site_domain']; ?>"><?php echo $settings['site_domain']; ?></a><br />
-							Заголовок портала: <?php echo $settings['site_name']; ?><br /><br />
-							
-							Имя сайта в Email: <?php echo $settings['site_email_name']; ?><br />
-							Email портала: <?php echo $settings['site_email']; ?><br /><br />
-							
-							Текущий шаблон: <?php echo $settings['site_template']; ?><br />
-							Новостей в базе: <?php echo NUM_NEWS; ?><br />
-							Проектов в базе: <?php echo NUM_PROJECTS; ?><br /><br />
-							
-							Администратор портала: <?php echo $users['user_name']; ?><br />
-							Email Администратора: <?php echo $users['user_email']; ?><br />
-							Ваш IP: <?php echo $_SERVER['REMOTE_ADDR']; ?>
-						</p>									
+							<?php if (isset($_POST['site_name'])) { echo $resultat."<br />Переадресация через 5 секунд."; header( 'Refresh: 5; url=acp_settings.html' ); } else { ?>
+												<!-- Form -->
+							<section>
+								<form name="set_settings" method="post" action="acp_settings.html">
+									<div class="row uniform 50%">
+										<div class="6u 12u$(xsmall)">
+											Заголовок сайта: <input type="text" name="site_name" id="site_name" value="<?php echo $settings['site_name'] ?>" />
+										</div>
+										<div class="6u 12u$(xsmall)">
+											Основной домен: <input type="text" name="site_domain" id="site_domain" value="<?php echo $settings['site_domain'] ?>" />
+										</div>
+										<div class="6u 12u$(xsmall)">
+											Текущий шаблон: <input type="text" name="site_template" id="site_template" value="<?php echo $settings['site_template'] ?>" />
+										</div>
+										<div class="6u 12u$(xsmall)">
+											Колличество просмотров: <input type="text" name="site_visits" id="site_visits" value="<?php echo $settings['site_visits'] ?>" />
+										</div>
+										<div class="6u 12u$(xsmall)">
+											Колличество уникальных посетителей: <input type="text" name="site_uniq_visits" id="site_uniq_visits" value="<?php echo $settings['site_uniq_visits'] ?>" />
+										</div>
+										<div class="6u 12u$(xsmall)">
+											Колличество постов на страницу: <input type="text" name="page_posts" id="page_posts" value="<?php echo $settings['page_posts'] ?>" />
+										</div>
+										<div class="6u 12u$(xsmall)">
+											Email портала: <input type="text" name="site_email" id="site_email" value="<?php echo $settings['site_email'] ?>" />
+										</div>
+										<div class="6u 12u$(xsmall)">
+											Пароль к Email'у портала: <input type="text" name="site_email_pass" id="site_email_pass" value="<?php echo $settings['site_email_pass'] ?>" />
+										</div>
+										<div class="6u 12u$(xsmall)">
+											Имя портала в Email: <input type="text" name="site_email_name" id="site_email_name" value="<?php echo $settings['site_email_name'] ?>" />
+										</div>
+										<div class="12u$">
+											<ul class="actions">
+												<li><input type="submit" value="Сохранить настройки" class="special" /></li>
+											</ul>
+										</div>
+									</div>
+								</form>
+							</section>		
+							<?php } ?>							
 					</div>
 
 							</div>
