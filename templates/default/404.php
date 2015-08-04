1425d197e3b408636982c9e4ebc4be3b439b7ae0
Makarin Nikolay <makarin@kgdnet.ru>
2015-08-04 13:25:41 +0200

    Fix redirect 404 page.

--- templates/default/404.php
+++ templates/default/404.php
@@ -14,6 +14,7 @@
 							<h2>Ошибка 404</h2>
 							<p>Извините,<br />
 							Такой страницы не существует!</p>
+							<?php header( 'Refresh: 5; url=main.html' ); ?>
 						</header>
 					</div>
 					<a href="main.html" class="goto-next scrolly">Вернуться назад</a>
