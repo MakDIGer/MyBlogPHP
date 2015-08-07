dfba55551205a20861deb7e2bcdbdd65c2552901
Makarin Nikolay <makarin@kgdnet.ru>
2015-08-07 23:27:40 +0200

    Fixed redirect

--- templates/default/email.php
+++ templates/default/email.php
@@ -13,6 +13,7 @@
 						<header>
 							<h2>Отправка сообщения</h2>
 							<p><?php echo $resultat; ?></p>
+							<?php header( 'Refresh: 5; url=main.html' ); ?>
 						</header>
 					</div>
 					<a href="main.html" class="goto-next scrolly">Вернуться назад</a>
