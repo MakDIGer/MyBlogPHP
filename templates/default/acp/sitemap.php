d7dfe932ca458b6300769a64e7b9bd3f178168d6
MakDIGer <makarin@kgdnet.ru>
2016-02-25 21:39:51 +0200

    Added Sitemap-generator

---
+++ templates/default/acp/sitemap.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * Персональный блог Макарина Николая
+ * Файл: templates/dafault/acp/sitemap.php
+ */
+
+?>
+							</div>
+							<div class="8u$ 12u$(medium) important(medium)">
+
+						<header class="major">
+							<h2>Обновление sitemap.xml</h2>
+							<p></p>
+						</header>
+						<p>
+							<?php echo $resultat."<br />Переадресация через 5 секунд."; header( 'Refresh: 5; url=acp_main.html' ); ?>
+						</p>									
+					</div>
+
+							</div>
+						</div>
+					</div>
+				</div>
